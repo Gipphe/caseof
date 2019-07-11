3b0578526655d3cf49c8ba85978136d5b9e4015e
Victor Nascimento Bakke <gipphe@gmail.com>
2019-07-12 01:26:13 +0200

    style: use es6
    BREAKING CHANGE: requires either transpilation for older environments
    and browsers, or requires you to stick to newer browsers and NodeJS
    versions.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,45 +1,45 @@
 import test from 'ava';
 import caseof from '../';
 
-test ('throws when supplied something other than a function as the first argument', (t) => {
+test ('throws when supplied something other than a function as the first argument', t => {
 	const err = t.throws (() => caseof ({}) (), Error);
 	t.is (err.message, 'First argument must be a function');
 });
 
-test ('calls matched function, and returns result of said function', (t) => {
-	const res = caseof ((when) => {
+test ('calls matched function, and returns result of said function', t => {
+	const res = caseof (when => {
 		when (x => typeof x === 'number') (x => x + 1);
 	}) (1);
 	t.is (res, 2);
 });
 
-test ('throws if none of the predicates matched', (t) => {
-	const fn = () => caseof ((when) => {
+test ('throws if none of the predicates matched', t => {
+	const fn = () => caseof (when => {
 		when (x => x === 2) (x => x + 1);
 	}) (3);
 	const err = t.throws (fn, Error);
 	t.is (err.message, 'None of the cases matches the value');
 });
 
-test ('returns result of first handler', (t) => {
-	const res = caseof ((when) => {
+test ('returns result of first handler', t => {
+	const res = caseof (when => {
 		when (x => typeof x === 'number') (x => x + 1);
 		when (x => typeof x === 'number') (x => x - 1);
 	}) (1);
 	t.is (res, 2);
 });
 
-test ('function on a pure side-effect basis', (t) => {
+test ('function on a pure side-effect basis', t => {
 	const foo = 'foo';
-	const res = caseof ((when) => {
+	const res = caseof (when => {
 		when (() => foo === 'bar') (() => 'bar');
 		when (() => foo === 'foo') (() => 'foo');
 	}) ();
 	t.is (res, 'foo');
 });
 
-test ('throws when `when` is passed more than 1 argument as a time', (t) => {
-	const fn = () => caseof ((when) => {
+test ('throws when `when` is passed more than 1 argument as a time', t => {
+	const fn = () => caseof (when => {
 		when (() => {}, () => {});
 	}) ();
 	const err = t.throws (fn, Error);
@@ -49,8 +49,8 @@ test ('throws when `when` is passed more than 1 argument as a time', (t) => {
 		'of "fn (x, y)"');
 });
 
-test ('caseOf.all returns all results', (t) => {
-	const res = caseof.all ((when) => {
+test ('caseOf.all returns all results', t => {
+	const res = caseof.all (when => {
 		when (x => x === 1) (x => x + 1);
 		when (x => x === 1) (x => x - 1);
 		when (x => typeof x !== 'number') (() => 'foo');
@@ -58,11 +58,11 @@ test ('caseOf.all returns all results', (t) => {
 	t.deepEqual (res, [2, 0]);
 });
 
-test ('only first matching case is run', (t) => {
+test ('only first matching case is run', t => {
 	let side1 = false;
 	let side2 = false;
 	let side3 = false;
-	caseof ((when) => {
+	caseof (when => {
 		when (() => true) (() => { side1 = true; });
 		when (() => true) (() => { side2 = true; });
 		when (() => true) (() => { side3 = true; });
@@ -72,8 +72,8 @@ test ('only first matching case is run', (t) => {
 	t.is (side3, false);
 });
 
-test ('default case with otherwise', (t) => {
-	const res = caseof ((when) => {
+test ('default case with otherwise', t => {
+	const res = caseof (when => {
 		when (x => x < 20) (x => x + 10);
 		when (caseof.otherwise) (() => 0);
 	}) (23);
