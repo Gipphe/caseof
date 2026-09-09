2ce7d340c578b512fe9fd7fa1f31a8f2c8714d12
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-09-09 10:20:10 +0200

    flake: remove package and checks
    Reduces maintenance overhead with updating `npmDepsHash`.

--- flake.nix
+++ flake.nix
@@ -4,50 +4,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
+    { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        packages = {
-          caseof = pkgs.buildNpmPackage {
-            pname = "caseof";
-            version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
-            src = ./.;
-            npmDepsHash = "sha256-3mtXOC75Vu6H6Y++Tn98uw7FvUiu9IEzg0/rIHpz63U=";
-          };
-          default = self.packages.${system}.caseof;
-        };
-
         devShells.default = pkgs.mkShell {
           packages = [ pkgs.nodejs_24 ];
-        };
-
-        checks = {
-          build = self.packages.x86_64-linux.caseof;
-
-          tests = self.packages.x86_64-linux.caseof.overrideAttrs {
-            name = "npm-test";
-            installPhase = /* bash */ ''
-              rm -rf "$out"
-              npm run test:coverage && touch "$out"
-            '';
-          };
-
-          fmt = self.packages.x86_64-linux.caseof.overrideAttrs {
-            name = "npm-fmt-check";
-            installPhase = /* bash */ ''
-              rm -rf "$out"
-              npm run fmt:check && touch "$out"
-            '';
-          };
         };
       }
     );
